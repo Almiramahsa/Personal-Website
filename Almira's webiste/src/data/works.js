d3b8253b1482653c9a0ed9d7eefecfb99e827f0b
Almira Mahsa <almiramahsa9@gmail.com>
2023-03-31 14:32:10 +0700

    edit works section

--- Almira's webiste/src/data/works.js
+++ Almira's webiste/src/data/works.js
@@ -1,18 +1,25 @@
 export default [
   {
+    title: 'Knowledge by CARI!',
+    description: ' Is an Automatic, Spatial, and Web-based Disaster Knowledge Search Engine (Main Portal).',
+    imgUrl: 'https://user-images.githubusercontent.com/53558772/222082694-89958535-e854-4b30-9cf6-54e5f9a38c9a.png',
+    tech: ['React', 'Tailwind', 'Vite', 'Leaflet'],
+  },
+  {
     title: 'Login-Register MERN',
+    description: 'Login-Register MERN is a web application that allows users to login and register using MongoDB, Express, React, and Node.js.',
     imgUrl: 'https://user-images.githubusercontent.com/53558772/222082694-89958535-e854-4b30-9cf6-54e5f9a38c9a.png',
     tech: ['React', 'CSS', 'Axios', 'Zustand', 'MogoDB', 'Express JSON', 'Nodejs', 'Figma'],
   },
   {
-    title: RawatInap,
-    description: 'An app where you can get hospital bed capacity information in real time, accurate, and transparant.',
+    title: 'RawatInap',
+    description: 'RawatInap is a app where you can get hospital bed capacity information in real time, accurate, and transparant.',
     imgUrl: 'https://user-images.githubusercontent.com/53558772/222078858-a30d6ae8-06c3-403f-a46e-88513b65a775.png',
     tech: ['React', 'Vite', 'Redux', 'Chakra UI', 'Tawilwind CSS'],
   },
   {
-    title: LuxSpace,
-    description: 'An app where you can get hospital bed capacity information in real time, accurate, and transparant.',
+    title: 'LuxSpace',
+    description: 'LuxSpace is a app where you can get hospital bed capacity information in real time, accurate, and transparant.',
     imgUrl: 'https://user-images.githubusercontent.com/53558772/222078463-1e3f0300-4640-4b92-af56-609c757bf14a.png',
     tech: ['React', 'Tailwind CSS', 'Figma'],
   },
