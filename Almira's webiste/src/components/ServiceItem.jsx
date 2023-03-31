d5b43bd6148ffbf138928bdee78e49f237764152
Almira Mahsa <almiramahsa9@gmail.com>
2023-03-31 14:52:04 +0700

    add slice for read more

--- Almira's webiste/src/components/ServiceItem.jsx
+++ Almira's webiste/src/components/ServiceItem.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ServiceItem({ title, icon, description }) {
+  const [showMore, setShowMore] = useState(false);
+
+  const shortDescription = description.split(' ').slice(0, 40).join(' ');
+  const fullDescription = description.split(' ').slice(0, 40).join(' ');
+
   return (
     <div className="px-5 ">
       <div className="bg-slate-300  hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 hover:-translate-y-2 p-5 transition transform rounded-md ">
         <div className="text-gray-800 dark:text-gray-200 w-6 h-6 mb-3">{icon}</div>
         <h1 className="text-gray-800 dark:text-gray-200 text-lg font-bold leading-6 truncate">{title}</h1>
-        <p className="text-gray-800 dark:text-gray-200 mb-5">{description}</p>
+        <p className="text-gray-800 dark:text-gray-200 mb-5">
+          {showMore ? description : shortDescription + '...'}
+          <button onClick={() => setShowMore(!showMore)} className="ml-2 text-blue-600 underline">
+            {showMore ? 'Read Less' : 'Read More'}
+          </button>
+        </p>
       </div>
     </div>
   );
--- Almira's webiste/src/data/services.jsx
+++ Almira's webiste/src/data/services.jsx
@@ -5,19 +5,19 @@ export default [
   {
     title: 'Web Design',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      "As your web designer, I'll closely collaborate with you to understand your needs and goals, and create visually stunning and user-friendly websites that reflect your brand and capture the attention of your target audience. I have a good understanding of design principles, color theory, and typography, and am proficient in tools such as Figma. If you're looking for a talented web designer to bring your vision to life, look no further!",
     icon: <MdWeb className=" w-full h-full" />,
   },
   {
     title: 'UI Design',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      "As your UI designer, I'll closely collaborate with you to understand your users' needs and goals, and create intuitive and engaging user interfaces for your website or app. I have a good understanding of user-centered design principles, and am proficient in tools such as Figma. If you're looking for a skilled UI designer to enhance the user experience of your digital product, let's chat!",
     icon: <VscSymbolColor className=" w-full h-full" />,
   },
   {
     title: 'Web Development',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      "As your web developer, I'll help you build high-quality and responsive websites that meet your business needs. I'll use my expertise in HTML, CSS, JavaScript, and other web technologies to create websites that are fast, reliable, and accessible. I have experience working with a variety of content management systems and frameworks, and am committed to delivering websites that adhere to the latest web standards and are optimized for search engines. If you're looking for a reliable web developer to bring your website to life, I am here to help!",
     icon: <VscCode className=" w-full h-full" />,
   },
 ];
